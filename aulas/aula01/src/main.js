0ebcfcc4821c13f462080adf058cb472557391c8
Gustavo von Sperling <guvon1982@gmail.com>
2026-03-19 21:51:53 -0300

    implemente validacao do login

--- aulas/aula01/src/main.js
+++ aulas/aula01/src/main.js
@@ -1,1 +1,34 @@
 import './style.css'
+
+const botao = document.querySelector("button");
+
+botao.addEventListener("click", (e) => {
+    const matriculaInput = document.querySelector("#matricula");
+    const matriculaErro = document.querySelector("#matriculaErro");
+    const senhaInput = document.querySelector("#senha");
+    const senhaErro = document.querySelector("#senhaErro");
+
+    matriculaErro.textContent = "";
+    senhaErro.textContent = "";
+
+
+    if(matriculaInput.value == ""){
+        matriculaErro.textContent = "Matrícula é obrigatória";
+        return;
+    }
+
+    if(parseInt(matriculaInput.value) <= 0){
+        matriculaErro.textContent = "Matrícula precisa ser um número positivo";
+        return;
+    }
+
+    if(senhaInput.value == ""){
+        senhaErro.textContent = "Senha é obrigatória";
+        return;
+    }
+    
+    if(senhaInput.value.length < 8){
+        senhaErro.textContent = "A senha deve conter pelo menos 8 caracters";
+        return;
+    }
+});
