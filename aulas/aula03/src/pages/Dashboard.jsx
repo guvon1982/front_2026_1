8248103d1b4c6206a58d45d3d4ffefcd7801e750
guvon <guvon1982@gmail.com>
2026-04-02 22:00:02 -0300

    ajustes aula 03

--- aulas/aula03/src/pages/Dashboard.jsx
+++ aulas/aula03/src/pages/Dashboard.jsx
@@ -5,15 +5,15 @@ function Dashboard() {
   return (
     <>
       <Layout>
-          <h2>Bem-vindo ao portal do aluno</h2>
-          <section>
-            <Card />
-            <Card />
-            <Card />
-          </section>
+        <h2>Bem-vindo ao portal do aluno</h2>
+        <section>
+          <Card />
+          <Card />
+          <Card />
+        </section>
       </Layout>
     </>
-);
+  );
 }
 
 export default Dashboard;
