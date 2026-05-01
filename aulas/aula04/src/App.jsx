59e5adc9a081f57d4fc36f90c90c2f69e41280b5
guvon <guvon1982@gmail.com>
2026-04-30 21:53:30 -0300

    aula04

---
+++ aulas/aula04/src/App.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router';
+import Settings from './pages/Settings';
+import Home from './pages/home';
+import About from './pages/About';
+import Erro404 from './pages/Erro404';
+
+
+function App(){
+  return (
+  <Routes>
+    <Route index element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/settings" element={<Settings />} />
+    <Route path="*" element={<Erro404 />} />
+  </Routes>
+  );
+}
+
+export default App;
