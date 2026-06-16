22896bd4f1b1d2d27ce1ca0bb9c2a97edb0e0eb7
guvon <guvon1982@gmail.com>
2026-06-16 20:23:40 -0300

    finalização aula06

--- aulas/aula06/src/context/AuthContext.jsx
+++ aulas/aula06/src/context/AuthContext.jsx
@@ -6,13 +6,15 @@ const AuthContext = createContext();
 
 //cria o provedor
 function AuthProvider({ children }) {
-  const [logado, setLogado] = useState(false);
+  const token = localStorage.getItem("app:token");
+  const [logado, setLogado] = useState(!!token);
   const [usuario, setUsuario] = useState({});
 
   const login = async (dados) => {
     const resposta = await autenticar(dados);
     if (resposta?.token) {
       setUsuario(resposta);
+      localStorage.setItem("app:token", JSON.stringify(resposta));
       setLogado(true);
       return;
     }
